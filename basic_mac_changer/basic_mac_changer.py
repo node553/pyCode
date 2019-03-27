8a9792edcffd4cf8d5bba322cb366dd870357891
node553 <node553@protonmail.com>
2019-03-27 19:30:37 +0000

    updated bugs

--- basic_mac_changer/basic_mac_changer.py
+++ basic_mac_changer/basic_mac_changer.py
@@ -1,46 +1,48 @@
 #!/usr/bin/env python
 
-# allows us to execute system commands
 import subprocess
-# module that allows you to get arguments from the user and parse them
 import optparse
+import re
 
 
-# this function defines what argurment can be called and it parses the input
 def get_arguments():
-    # parser object that handles user input
     parser = optparse.OptionParser()
-    # giving the object the interface argument it can expect from users
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
-    # giving the object the new MAC argument it can expect from users
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-    # telling the object to parse the arguments it gets from the user
-    # returns all the info you get to wherever you call it
     (options, arguments) = parser.parse_args()
-    # checking if they passed all the arguments needed
     if not options.interface:
-        # code to handle if no interface argument is passed
         parser.error("[-] Please specify an interface, use --help or -h for more info")
     elif not options.new_mac:
-         # code to handle if no MAC address argument is passed
         parser.error("[-] Please specify a MAC address, use --help or -h for more info")
     return options
 
 
-# this function uses the subprocess.call() method to change the MAC addr
 def change_mac_addr(interface, new_mac):
-    # tells the user what the f**k is happening
     print("[+] Changing MAC Address on " + interface + " to " + new_mac)
 
-    # system commands needed to change the MAC address
-    # everytime there is a space you create a new item in the list
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
 
 
+def get_mac_addr(interface):
+    ifconfig_res = subprocess.check_output(["ifconfig", interface])
+    # getting mac addr using regular expressions
+    mac_addr_res = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_res)
 
-# setting options variable to the get arguments function
+    if mac_addr_res:
+        return mac_addr_res.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
 options = get_arguments()
-# calling the change_mac_addr function and passing the variables from the object
+current_mac = get_mac_addr(options.interface)
+print("Current MAC address = " + str(current_mac))
 change_mac_addr(options.interface, options.new_mac)
+
+current_mac = get_mac_addr(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC was changed to: " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
